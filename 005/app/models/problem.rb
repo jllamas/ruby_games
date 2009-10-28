6667a9c9ea2795875ff7f3d194976b93aabb2198
Jacob Llamas <jllamas@bender.ipac.caltech.edu>
2009-10-28 11:03:39 -0700

    Added new ruby game 005 - Jake

---
+++ 005/app/models/problem.rb
@@ -0,0 +1,12 @@
+class Problem < ActiveResource::Base
+  
+  def self.bridgesuits( shuffled )
+
+    # example of bridge order lowest to highest: Ace Clubs, Ace Diamonds, Ace Hearts, Ace Spades, 2 Clubs, 2 Diamonds, 2 Hearts, 2 Spades, 3 Clubs, 3 Diamonds, 3 Hearts, 3 Spades
+    
+    #sorted = [ "Ace Clubs", "Ace Diamonds", "Ace Hearts", "Ace Spades", "2 Clubs", "2 Diamonds", "2 Hearts", "2 Spades", "3 Clubs", "3 Diamonds", "3 Hearts", "3 Spades", "4 Clubs", "4 Diamonds", "4 Hearts", "4 Spades", "5 Clubs", "5 Diamonds", "5 Hearts", "5 Spades", "6 Clubs", "6 Diamonds", "6 Hearts", "6 Spades", "7 Clubs", "7 Diamonds", "7 Hearts", "7 Spades", "8 Clubs", "8 Diamonds", "8 Hearts", "8 Spades", "9 Clubs", "9 Diamonds", "9 Hearts", "9 Spades", "10 Clubs", "10 Diamonds", "10 Hearts", "10 Spades", "Jack Clubs", "Jack Diamonds", "Jack Hearts", "Jack Spades", "Queen Clubs", "Queen Diamonds", "Queen Hearts", "Queen Spades", "King Clubs", "King Diamonds", "King Hearts", "King Spades" ]
+
+  end
+
+end
+
